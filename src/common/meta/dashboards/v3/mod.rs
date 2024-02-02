48255ecf45962ceba607bd5388cf3352ba08bfde
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2024-02-02 11:47:18 +0530

    backend

--- src/common/meta/dashboards/v3/mod.rs
+++ src/common/meta/dashboards/v3/mod.rs
@@ -105,6 +105,10 @@ pub struct PanelFields {
     pub longitude: Option<AxisItem>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub weight: Option<AxisItem>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub name: Option<AxisItem>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub value: Option<AxisItem>,
     pub filter: Vec<PanelFilter>,
 }
 
