feb5dc3058cef5d3e232c52a4b4749f18e55c54d
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2024-02-02 11:00:02 +0530

    changed from v2 to v3

--- src/common/meta/dashboards/v3/mod.rs
+++ src/common/meta/dashboards/v3/mod.rs
@@ -170,6 +170,8 @@ pub struct PanelConfig {
     legend_width: Option<LegendWidth>,
     base_map: Option<BaseMap>,
     map_view: Option<MapView>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    map_symbol_style: Option<MapSymbolStyle>,
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, ToSchema)]
@@ -234,6 +236,19 @@ pub struct MapView {
     pub zoom: f64,
     pub lat: f64,
     pub lng: f64,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, ToSchema)]
+pub struct MapSymbolStyle {
+    pub size: String,
+    pub size_by_value: Option<SizeByValue>,
+    pub size_fixed: f64,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, ToSchema)]
+pub struct SizeByValue {
+    pub min: f64,
+    pub max: f64,
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, ToSchema)]
